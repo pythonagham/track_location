365bfbf5a149dcbc684c9304781c1f448428d5b2
Nagham Fakih <pythonagham@gmail.com>
2024-05-31 11:54:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ root.title("Phone Number Tracker")
 root.geometry("385x594+300+200")
 root.resizable(False,False)
 root.configure(bg='#96BFFF')
-key="d2b3af88e38b4b5bb6e04f77a89e39f3"
+key="get your free api key from openCage website"
 
 def track():
     enter_nb=entry.get()
